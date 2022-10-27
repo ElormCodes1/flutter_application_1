4ae7f84ed2f1de4c8ccfc4e36fcea9c0d39b76cf
ElormCodes1 <107018207+ElormCodes1@users.noreply.github.com>
2022-10-27 17:18:58 +0000

    first day practice from utube video

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      const MaterialApp(
-        home: Center(
-          child: Text('Hello World'),
+//main function, starting point for the flutter app
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Text('I am Rich'),
+          centerTitle: true,
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image:
+                NetworkImage('https://www.w3schools.com/w3css/img_lights.jpg'),
+          ),
         ),
       ),
-    );
+    ),
+  );
+}
